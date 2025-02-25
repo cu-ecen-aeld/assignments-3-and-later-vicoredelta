9e15c8fed0de589ddb7e3eabe52993e5e57710a9
Emil <vicoredelta@proton.me>
2025-02-25 10:38:56 +0100

    do tasks for assignment 2

---
+++ finder-app/writer.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <syslog.h>
+
+int main(int argc, char* argv[])
+{
+	FILE *fptr;
+	char* filename;
+	char* writestr;
+
+	//syslog(LOG_USER, "Writing utility running.");
+
+	if (argc != 3)
+	{
+		syslog(LOG_ERR, "Utility takes two arguments.");
+		return 1;
+	}
+
+	filename = argv[1];
+	writestr = argv[2];
+	printf("Writing %s to file %s\n", writestr, filename);
+
+	//syslog(LOG_DEBUG, "Writing %s to file %s", writestr, filename);
+	fptr = fopen(filename, "w+");
+	fprintf(fptr, "%s", writestr);
+
+	fclose(fptr);
+
+	return 0;
+}
