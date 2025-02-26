c509acc2bab6f55929a40a731922a5699ef83db8
Emil <vicoredelta@proton.me>
2025-02-26 14:16:38 +0100

    complete assignment 3 part 1

---
+++ examples/systemcalls/systemcalls.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+bool do_system(const char *command);
+
+bool do_exec(int count, ...);
+
+bool do_exec_redirect(const char *outputfile, int count, ...);
